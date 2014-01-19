f7ce23ef4ea33106494a47571fce8a20180609fd
zaininnari <zaininnari@gmail.com>
2014-01-19 23:14:47 +0900

    fix

--- attributes/mysql-connectors-community.rb
+++ attributes/mysql-connectors-community.rb
@@ -1,5 +1,5 @@
 default['yum']['mysql-connectors-community']['repositoryid'] = 'mysql-connectors-community'
-default['yum']['mysql-connectors-community']['mirrorlist'] = 'http://repo.mysql.com/yum/mysql-connectors-community/el/6/$basearch/'
+default['yum']['mysql-connectors-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-connectors-community/el/6/$basearch/'
 default['yum']['mysql-connectors-community']['description'] = 'MySQL Connectors Community'
 default['yum']['mysql-connectors-community']['enabled'] = true
 default['yum']['mysql-connectors-community']['gpgcheck'] = true
--- attributes/mysql-tools-community.rb
+++ attributes/mysql-tools-community.rb
@@ -1,5 +1,5 @@
 default['yum']['mysql-tools-community']['repositoryid'] = 'mysql-tools-community'
-default['yum']['mysql-tools-community']['mirrorlist'] = 'http://repo.mysql.com/yum/mysql-tools-community/el/6/$basearch/'
+default['yum']['mysql-tools-community']['baseurl'] = 'http://repo.mysql.com/yum/mysql-tools-community/el/6/$basearch/'
 default['yum']['mysql-tools-community']['description'] = 'MySQL Tools Community'
 default['yum']['mysql-tools-community']['enabled'] = true
 default['yum']['mysql-tools-community']['gpgcheck'] = true
